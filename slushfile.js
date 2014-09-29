08c5acb44342ee72f16e8d6cb56a530f355cadca
Julian Pineda <julian.pineda@ontario.ca>
2014-09-28 20:31:49 -0400

    commenting promps for testing purpose

--- slushfile.js
+++ slushfile.js
@@ -42,29 +42,30 @@ gulp.task('default', function (done) {
         name: 'appName',
         message: 'What is the name of your project?',
         default: defaults.appName
-    }, {
-        name: 'appDescription',
-        message: 'What is the description?'
-    }, {
-        name: 'appVersion',
-        message: 'What is the version of your project?',
-        default: '0.1.0'
-    }, {
-        name: 'authorName',
-        message: 'What is the author name?',
-    }, {
-        name: 'authorEmail',
-        message: 'What is the author email?',
-        default: defaults.authorEmail
-    }, {
-        name: 'userName',
-        message: 'What is the github username?',
-        default: defaults.userName
-    }, {
-        type: 'confirm',
-        name: 'moveon',
-        message: 'Continue?'
-    }];
+    // }, {
+    //     name: 'appDescription',
+    //     message: 'What is the description?'
+    // }, {
+    //     name: 'appVersion',
+    //     message: 'What is the version of your project?',
+    //     default: '0.1.0'
+    // }, {
+    //     name: 'authorName',
+    //     message: 'What is the author name?',
+    // }, {
+    //     name: 'authorEmail',
+    //     message: 'What is the author email?',
+    //     default: defaults.authorEmail
+    // }, {
+    //     name: 'userName',
+    //     message: 'What is the github username?',
+    //     default: defaults.userName
+    // }, {
+    //     type: 'confirm',
+    //     name: 'moveon',
+    //     message: 'Continue?'
+    // }
+    ];
     //Ask
     inquirer.prompt(prompts,
         function (answers) {
