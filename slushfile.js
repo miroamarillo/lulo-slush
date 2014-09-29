1de4ecd7baee61a908c49b8298bb9ff03bd2ef61
Julian Pineda <julian.pineda@ontario.ca>
2014-09-28 22:19:44 -0400

    correcting bugs.  Adding more questions.

--- slushfile.js
+++ slushfile.js
@@ -17,11 +17,11 @@ var gulp = require('gulp'),
     inquirer = require('inquirer');
 
 function format(string) {
-    var username = string.toLowerCase();
+    var username = string ? string.toLowerCase() : '';
     return username.replace(/\s/g, '');
 }
 
-var defaults = (function () {
+var defaults = (function() {
     var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
         workingDirName = process.cwd().split('/').pop().split('\\').pop(),
         osUserName = homeDir && homeDir.split('/').pop() || 'root',
@@ -37,45 +37,42 @@ var defaults = (function () {
     };
 })();
 
-gulp.task('default', function (done) {
+gulp.task('default', function(done) {
     var prompts = [{
         name: 'appName',
         message: 'What is the name of your project?',
         default: defaults.appName
-    // }, {
-    //     name: 'appDescription',
-    //     message: 'What is the description?'
-    // }, {
-    //     name: 'appVersion',
-    //     message: 'What is the version of your project?',
-    //     default: '0.1.0'
-    // }, {
-    //     name: 'authorName',
-    //     message: 'What is the author name?',
-    // }, {
-    //     name: 'authorEmail',
-    //     message: 'What is the author email?',
-    //     default: defaults.authorEmail
-    // }, {
-    //     name: 'userName',
-    //     message: 'What is the github username?',
-    //     default: defaults.userName
-    // }, {
-    //     type: 'confirm',
-    //     name: 'moveon',
-    //     message: 'Continue?'
-    // }
+    }, {
+        name: 'appDescription',
+        message: 'What is the description?'
+    }, {
+        name: 'appVersion',
+        message: 'What is the version of your project?',
+        default: '0.1.0'
+    }, {
+        name: 'authorName',
+        message: 'What is the author name?',
+    }, {
+        name: 'authorEmail',
+        message: 'What is the author email?',
+        default: defaults.authorEmail
+    }, {
+        name: 'userName',
+        message: 'What is the github username?',
+        default: defaults.userName
+    }, {
+        type: 'confirm',
+        name: 'moveon',
+        message: 'Continue?'
+    }
     ];
     //Ask
     inquirer.prompt(prompts,
-        function (answers) {
-            if (!answers.moveon) {
-                return done();
-            }
+        function(answers) {
             answers.appNameSlug = _.slugify(answers.appName);
             gulp.src(__dirname + '/templates/**')
                 .pipe(template(answers))
-                .pipe(rename(function (file) {
+                .pipe(rename(function(file) {
                     if (file.basename[0] === '_') {
                         file.basename = '.' + file.basename.slice(1);
                     }
@@ -83,7 +80,7 @@ gulp.task('default', function (done) {
                 .pipe(conflict('./'))
                 .pipe(gulp.dest('./'))
                 .pipe(install())
-                .on('end', function () {
+                .on('end', function() {
                     done();
                 });
         });
